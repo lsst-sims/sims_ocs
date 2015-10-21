acd692dd6627a1dd8c1fcd90dc3c723a9c13f1e9
Michael Reuter <mareuter@lsst.org>
2015-10-21 14:25:16 -0700

    More testing of Simulator.

--- tests/test_simulator.py
+++ tests/test_simulator.py
@@ -1,5 +1,10 @@
 import unittest
 
+try:
+    from unittest import mock
+except ImportError:
+    import mock
+
 from lsst.sims.ocs.kernel.simulator import Simulator
 
 class SimulatorTest(unittest.TestCase):
@@ -14,3 +19,10 @@ class SimulatorTest(unittest.TestCase):
     def test_fraction_overwrite(self):
         self.sim.fractional_duration = 1.0 / 365.0
         self.assertEqual(self.sim.duration, 1.0)
+
+    @mock.patch("lsst.sims.ocs.sal.sal_manager.SalManager.set_subscribe_topic")
+    @mock.patch("lsst.sims.ocs.sal.sal_manager.SalManager.set_publish_topic")
+    def test_initialization(self, mock_salmanager_pub_topic, mock_salmanager_sub_topic):
+        self.sim.initialize()
+        self.assertEqual(mock_salmanager_pub_topic.call_count, 2)
+        self.assertEqual(mock_salmanager_sub_topic.call_count, 1)
