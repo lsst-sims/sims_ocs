c6c285e3b9fff5c510a063099de856653152bed3
Michael Reuter <mareuter@lsst.org>
2015-10-23 13:06:31 -0700

    Increasing Simulator code coverage.

--- tests/test_simulator.py
+++ tests/test_simulator.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import unittest
 
 try:
@@ -12,26 +13,33 @@ class SimulatorTest(unittest.TestCase):
     def setUp(self):
         import collections
 
-        options = collections.namedtuple("options", ["frac_duration", "no_scheduler"])
-        options.frac_duration = 0.5
-        options.no_scheduler = False
+        self.options = collections.namedtuple("options", ["frac_duration", "no_scheduler"])
+        self.options.frac_duration = 0.5
+        self.options.no_scheduler = True
 
         lsst_survey = collections.namedtuple("lsst_survey", ["start_date", "duration"])
         lsst_survey.start_date = "2020-05-24"
         lsst_survey.duration = 1.0
 
-        configuration = collections.namedtuple("configuration", ["lsst_survey"])
-        configuration.lsst_survey = lsst_survey
+        self.configuration = collections.namedtuple("configuration", ["lsst_survey"])
+        self.configuration.lsst_survey = lsst_survey
 
-        self.sim = Simulator(options, configuration)
+        self.sim = Simulator(self.options, self.configuration)
 
     def test_initial_creation(self):
         self.assertEqual(self.sim.duration, 183.0)
         self.assertEqual(self.sim.time_handler.initial_timestamp, 1590278400.0)
+        self.assertEqual(self.sim.seconds_in_night, 10 * 3600)
+        self.assertEqual(self.sim.hours_in_daylight, 14)
 
     def test_fraction_overwrite(self):
-        self.sim.fractional_duration = 1.0 / 365.0
+        self.sim.fractional_duration = 1 / 365
         self.assertEqual(self.sim.duration, 1.0)
+
+    def test_no_override_from_options(self):
+        self.options.frac_duration = -1
+        sim = Simulator(self.options, self.configuration)
+        self.assertEquals(sim.duration, 365.0)
 
     @mock.patch("lsst.sims.ocs.kernel.sequencer.Sequencer.initialize")
     @mock.patch("lsst.sims.ocs.sal.sal_manager.SalManager.set_subscribe_topic")
@@ -46,3 +54,47 @@ class SimulatorTest(unittest.TestCase):
     def test_finalization(self, mock_salmanager_final):
         self.sim.finalize()
         self.assertEqual(mock_salmanager_final.call_count, 1)
+
+    def short_run(self, wait_for_sched):
+        # Setup for 1 night and 9 visits
+        self.num_visits = 9
+        # One for timestamp and one for observation
+        self.put_calls = 2 * self.num_visits
+        self.sim.fractional_duration = 1 / 365
+        self.sim.wait_for_scheduler = wait_for_sched
+        self.sim.hours_in_night = 0.1
+        self.assertEquals(self.sim.duration, 1.0)
+        self.assertEquals(self.sim.seconds_in_night, 360)
+
+    @mock.patch("lsst.sims.ocs.sal.sal_manager.SalManager.put")
+    @mock.patch("lsst.sims.ocs.sal.sal_manager.SalManager.set_subscribe_topic")
+    @mock.patch("lsst.sims.ocs.sal.sal_manager.SalManager.set_publish_topic")
+    def test_run_no_scheduler(self, mock_salmanager_pub_topic, mock_salmanager_sub_topic,
+                              mock_salmanager_put):
+        self.short_run(False)
+
+        self.sim.initialize()
+        self.sim.run()
+
+        self.assertEquals(mock_salmanager_put.call_count, self.put_calls)
+        self.assertEquals(self.sim.seq.targets_received, self.num_visits)
+        self.assertEquals(self.sim.seq.observations_made, self.num_visits)
+
+    @mock.patch("SALPY_scheduler.SAL_scheduler.getNextSample_targetTest")
+    @mock.patch("lsst.sims.ocs.sal.sal_manager.SalManager.put")
+    @mock.patch("lsst.sims.ocs.sal.sal_manager.SalManager.set_subscribe_topic")
+    @mock.patch("lsst.sims.ocs.sal.sal_manager.SalManager.set_publish_topic")
+    def test_run_with_scheduler(self, mock_salmanager_pub_topic, mock_salmanager_sub_topic,
+                                mock_salmanager_put, mock_salmanager_get):
+        self.short_run(True)
+        get_calls = 1 * self.num_visits
+        mock_salmanager_get.return_value = 0
+
+        self.sim.initialize()
+        self.sim.target.num_exposures = 2
+        self.sim.run()
+
+        self.assertEquals(mock_salmanager_put.call_count, self.put_calls)
+        self.assertEquals(mock_salmanager_get.call_count, get_calls)
+        self.assertEquals(self.sim.seq.targets_received, self.num_visits)
+        self.assertEquals(self.sim.seq.observations_made, self.num_visits)
