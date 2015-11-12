c919b4863e80e11a7a7f877a4fc9e51c44adbcdf
Michael Reuter <mareuternh@gmail.com>
2015-11-12 10:46:49 -0700

    Standardize format and unit layout in doc string.

--- lsst/sims/ocs/configuration/lsst_survey.py
+++ lsst/sims/ocs/configuration/lsst_survey.py
@@ -5,8 +5,8 @@ __all__ = ["LsstSurvey"]
 class LsstSurvey(pexConfig.Config):
     """Configuration for the LSST Survey.
     """
-    start_date = pexConfig.Field("The start date of the LSST Survey. Format is YYYY-MM-DD.", str)
-    duration = pexConfig.Field("The fractional duration in years of the survey.", float)
+    start_date = pexConfig.Field("The start date (format=YYYY-MM-DD) of the LSST Survey.", str)
+    duration = pexConfig.Field("The fractional duration (units=years) of the survey.", float)
 
     def setDefaults(self):
         """Set defaults for the LSST Survey.
