79ebc10b0481417f2e9da36ee09ff507516fde9e
Michael Reuter <mareuternh@gmail.com>
2015-11-16 15:48:50 -0700

    Adding my.cnf config file path for MySQL mode.

--- lsst/sims/ocs/database/socs_db.py
+++ lsst/sims/ocs/database/socs_db.py
@@ -8,18 +8,23 @@ from ..utilities.session_info import get_hostname
 
 class SocsDatabase(object):
 
-    def __init__(self, dialect="mysql"):
+    def __init__(self, dialect="mysql", mysql_config_path=None):
         self.db_name = "SocsDB"
         self.db_dialect = dialect
         self.metadata = MetaData()
         self.engine = None
         self.db_save_dir = ""
+        self.mysql_config_path = mysql_config_path
 
         tbl = create_session(self.metadata)
         setattr(self, tbl.name.lower(), tbl)
 
     def _connect(self):
-        conf_file = os.path.join(os.getenv("HOME"), ".my.cnf")
+        if self.mysql_config_path is not None:
+            conf_path = self.mysql_config_path
+        else:
+            conf_path = os.getenv("HOME")
+        conf_file = os.path.join(conf_path, ".my.cnf")
         return mysql.connect(read_default_file=conf_file, db=self.db_name)
 
     def _make_engine(self, initialize=True):
