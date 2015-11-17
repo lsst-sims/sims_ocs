7ad629bc424541cb639574dc190ec97d97187799
Michael Reuter <mareuternh@gmail.com>
2015-11-17 09:21:36 -0700

    Adding documentation for class and function.

--- lsst/sims/ocs/database/socs_db.py
+++ lsst/sims/ocs/database/socs_db.py
@@ -7,8 +7,21 @@ from .tables.base_tbls import create_session
 from ..utilities.session_info import get_hostname
 
 class SocsDatabase(object):
+    """Main class for simulation database interaction.
+
+    This class is responsible for interacting with the main simulation database. For MySQL, this is a central
+    database containing all the relevant tables. For SQLite, this consists of a machine session tracking
+    database and individual database files for each run session of the simulation.
+    """
 
     def __init__(self, dialect="mysql", mysql_config_path=None, sqlite_save_path=None):
+        """Class constructor.
+
+        Args:
+            dialect (str): The flavor of the database to use. Options: mysql or sqlite.
+            mysql_config_path (str): An alternate path for the .my.cnf configuration file for MySQL.
+            sqlite_save_path (str): A path to save all resulting database files for SQLite.
+        """
         self.db_name = "SocsDB"
         self.db_dialect = dialect
         self.metadata = MetaData()
@@ -22,9 +35,19 @@ class SocsDatabase(object):
             self.session_tracking = create_session(self.metadata)
 
     def _create_tables(self, use_autoincrement=True):
+        """Create all the relevant tables.
+
+        Args:
+            use_autoincrement (bool): A flag to set auto increment behavior on the Session table.
+        """
         self.session = create_session(self.metadata, use_autoincrement)
 
     def _connect(self):
+        """Create the database connection for MySQL.1
+
+        Returns:
+            (function): The connection function for MySQL.
+        """
         if self.mysql_config_path is not None:
             conf_path = self.mysql_config_path
         else:
@@ -33,6 +56,11 @@ class SocsDatabase(object):
         return mysql.connect(read_default_file=conf_file, db=self.db_name)
 
     def _make_engine(self, sqlite_db=None):
+        """Create the engine for database interactions.
+
+        Args:
+            sqlite_db (str): The name of the database file for SQLite.
+        """
         if self.engine is None:
             if self.db_dialect == "mysql":
                 self.engine = create_engine("mysql://", creator=self._connect)
@@ -40,6 +68,12 @@ class SocsDatabase(object):
                 self.engine = create_engine("sqlite:///{}".format(sqlite_db))
 
     def create_db(self):
+        """Create the database tables.
+
+        This function does the actual work of creating all the relevant database tables. For MySQL, these
+        go into the central database. For SQLite, this creates the session tracking database with the Session
+        table.
+        """
         if self.db_dialect == "sqlite":
             sqlite_session_tracking_db = "{}_sessions.db".format(get_hostname())
             if self.sqlite_save_path is not None:
@@ -50,5 +84,9 @@ class SocsDatabase(object):
         self.metadata.create_all(self.engine)
 
     def delete_db(self):
+        """Delete all database tables.
+
+        This function only works for MySQL. It will have no effect on SQLite.
+        """
         self._make_engine()
         self.metadata.drop_all(self.engine)
--- lsst/sims/ocs/database/tables/base_tbls.py
+++ lsst/sims/ocs/database/tables/base_tbls.py
@@ -3,6 +3,18 @@ from sqlalchemy.types import DATETIME
 from sqlalchemy import DDL, event
 
 def create_session(metadata, autoincrement=True):
+    """Create Session table.
+
+    This function creates the Session table for tracking the various simulations run. For MySQL, it adds
+    a post-create command to set the lower limit of the auto increment value.
+
+    Args:
+        metadata (sqlalchemy.MetaData): The database object that collects the tables.
+        autoincrement (bool): A flag to set auto incrementing on the sessionID column.
+
+    Returns:
+        (sqlalchemy.Table): The Session table object.
+    """
     table = Table("Session", metadata,
                   Column("sessionID", Integer, primary_key=True, autoincrement=autoincrement, nullable=False),
                   Column("sessionUser", String(80), nullable=False),
