821f7107d22b55cce395abffb96fdd2b4d14cd0a
Michael Reuter <mareuter@lsst.org>
2016-01-21 13:25:49 -0700

    Adding delay in field loop so correct number comes across.

--- lsst/sims/ocs/kernel/simulator.py
+++ lsst/sims/ocs/kernel/simulator.py
@@ -1,3 +1,4 @@
+import time
 import logging
 
 from lsst.sims.ocs.configuration.conf_comm import ConfigurationCommunicator
@@ -145,6 +146,7 @@ class Simulator(object):
                         end_fields = True
                         continue
                 self.field_list.append(write_field(self.field))
+                time.sleep(0.0005)
             self.log.info("{} fields retrieved".format(len(self.field_list)))
             self.db.write_table("field", self.field_list)
 
