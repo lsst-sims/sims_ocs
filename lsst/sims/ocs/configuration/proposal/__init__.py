f3dc6ad4a8b77fbec6c43367a0fda1e486caed93
Michael Reuter <mareuter@lsst.org>
2016-06-14 12:10:33 -0700

    Removing galactic exclusion config class.

--- lsst/sims/ocs/configuration/proposal/__init__.py
+++ lsst/sims/ocs/configuration/proposal/__init__.py
@@ -2,7 +2,6 @@
 Module for classes that handle the configuration of the science proposals.
 """
 from .band_filter import *
-from .galactic_exclusion import *
 from .scheduling import *
 from .selection import *
 from .sky_constraints import *
