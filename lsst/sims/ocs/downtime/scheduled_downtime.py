2733d52480023cbeae0bc12c8718b9d7ea2a3c89
Michael Reuter <mareuter@lsst.org>
2016-06-15 15:02:44 -0700

    Adding initial scheduled downtime information.

---
+++ lsst/sims/ocs/downtime/scheduled_downtime.py
@@ -0,0 +1,60 @@
+import pkg_resources
+import sqlite3
+
+class ScheduledDowntime(object):
+    """Handle the scheduled downtime information.
+
+    This class handles the scheduled downtime information.
+    """
+
+    SCHEDULED_DOWNTIME_DB = "scheduled_downtime.db"
+
+    def __init__(self):
+        """Initialize the class.
+        """
+        self.downtime_file = None
+        self.downtimes = []
+
+    def __len__(self):
+        """Return number of scheduled downtimes.
+
+        Returns
+        -------
+        int
+        """
+        return len(self.downtimes)
+
+    def initialize(self, downtime_file=None):
+        """Configure the set of scheduled downtimes.
+
+        This function gets the appropriate database file and creates the set of
+        scheduled downtimes from it. The default behavior is to use the module stored
+        database. However, an alternate database file can be provided. The alternate
+        database file needs to have a table called *Downtime* with the following columns:
+
+        night
+            int : The night (from start of simulation) the downtime occurs.
+        duration
+            int : The duration (units=days) of the downtime.
+        activity
+            str : A description of the activity involved.
+
+        Parameters
+        ----------
+        downtime_file : str
+            A full path to an alternate scheduled downtime database file.
+        """
+
+        if downtime_file is not None:
+            self.downtime_file = downtime_file
+        else:
+            rsman = pkg_resources.ResourceManager()
+            self.downtime_file = rsman.resource_filename("lsst.sims.ocs",
+                                                         "downtime/{}".format(self.SCHEDULED_DOWNTIME_DB))
+
+        with sqlite3.connect(self.downtime_file) as conn:
+            cur = conn.cursor()
+            cur.execute("select * from Downtime;")
+            for row in cur:
+                self.downtimes.append((row[0], row[1], row[2]))
+            cur.close()
---
+++ tests/downtime/test_scheduled_downtime.py
@@ -0,0 +1,48 @@
+import os
+import sqlite3
+import unittest
+
+from lsst.sims.ocs.downtime.scheduled_downtime import ScheduledDowntime
+
+class ScheduledDowntimeTest(unittest.TestCase):
+
+    def setUp(self):
+        self.sdt = ScheduledDowntime()
+
+    def check_downtime(self, downtime, night, duration, activity):
+        self.assertEqual(downtime[0], night)
+        self.assertEqual(downtime[1], duration)
+        self.assertEqual(downtime[2], activity)
+
+    def test_basic_information_after_creation(self):
+        self.assertIsNone(self.sdt.downtime_file)
+        self.assertEqual(len(self.sdt), 0)
+
+    def test_information_after_initialization(self):
+        self.sdt.initialize()
+        self.assertEquals(os.path.basename(self.sdt.downtime_file), "scheduled_downtime.db")
+        self.assertEqual(len(self.sdt), 31)
+        self.check_downtime(self.sdt.downtimes[0], 158, 7, "general maintenance")
+        self.check_downtime(self.sdt.downtimes[-1], 7242, 7, "general maintenance")
+
+    def test_alternate_db(self):
+        downtime_dbfile = "alternate_scheduled_downtime.db"
+
+        downtime_table = []
+        downtime_table.append("night INTEGER PRIMARY KEY")
+        downtime_table.append("duration INTEGER")
+        downtime_table.append("activity TEXT")
+
+        conn = sqlite3.connect(downtime_dbfile)
+        with conn:
+            cur = conn.cursor()
+            cur.execute("DROP TABLE IF EXISTS Downtime")
+            cur.execute("CREATE TABLE Downtime({})".format(",".join(downtime_table)))
+            cur.execute("INSERT INTO Downtime VALUES(?, ?, ?)", (100, 7, "something to do"))
+            cur.close()
+
+        self.sdt.initialize(downtime_dbfile)
+        self.assertEqual(len(self.sdt), 1)
+        self.check_downtime(self.sdt.downtimes[0], 100, 7, "something to do")
+
+        os.remove(downtime_dbfile)
