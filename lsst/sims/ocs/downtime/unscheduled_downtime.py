6753dfc144e5726cca9957bb9a10aad3f1fbd9fc
Michael Reuter <mareuter@lsst.org>
2016-06-16 09:57:43 -0700

    Adding initial unscheduled downtime information.

---
+++ lsst/sims/ocs/downtime/unscheduled_downtime.py
@@ -0,0 +1,97 @@
+import logging
+import random
+import time
+
+class UnscheduledDowntime(object):
+    """Handle creating the unscheduled downtime information.
+
+    This class handles the unscheduled downtime information.
+    """
+
+    MINOR_EVENT = (0.0137, 1, "minor event")
+    INTERMEDIATE_EVENT = (0.00548, 3, "intermediate event")
+    MAJOR_EVENT = (0.00137, 7, "major event")
+    CATASTROPHIC_EVENT = (0.000274, 14, "catastrophic event")
+
+    def __init__(self):
+        """Initialize the class.
+        """
+        self.seed = 1640995200
+        self.downtimes = []
+        self.log = logging.getLogger("downtime.UnscheduledDowntime")
+
+    def __len__(self):
+        """Return number of scheduled downtimes.
+
+        Returns
+        -------
+        int
+        """
+        return len(self.downtimes)
+
+    @property
+    def total_downtime(self):
+        """Get the total downtime (units=days).
+
+        Returns
+        -------
+        int
+        """
+        return sum([x[1] for x in self.downtimes])
+
+    def initialize(self, survey_length=3650, random_seed=False):
+        """Configure the set of unscheduled downtimes.
+
+        This function creates the unscheduled downtimes based on a set of probabilities
+        of the downtime type occurance. A default seed is used to produce the same set of
+        downtimes, but a randomized seed can be requested.
+
+        The random downtime is calculated using the following probabilities:
+        minor event (remainder of night and next day) = 5/365 days
+              e.g. power supply failure
+        intermediate (3 nights) = 2/365 days
+              e.g. repair filter mechanism, rotator, hexapod, or shutter
+        major event (7 nights) = 1/2*365 days
+        catastrophic event (14 nights) = 1/3650 days
+              e.g. replace a raft
+
+        Parameters
+        ----------
+        survey_length : int
+            The length of the survey in days. Default is the length of a 10 year survey.
+        random_seed : bool
+            Flag to set the seed based on the current time. Default is to used fixed seed.
+        """
+        if random_seed:
+            self.seed = int(time.time())
+
+        random.seed(self.seed)
+
+        nights = 0
+        while nights < survey_length:
+            prob = random.random()
+            if prob < self.CATASTROPHIC_EVENT[0]:
+                self.downtimes.append((nights, self.CATASTROPHIC_EVENT[1], self.CATASTROPHIC_EVENT[2]))
+                nights += self.CATASTROPHIC_EVENT[1] + 1
+                continue
+            else:
+                prob = random.random()
+                if prob < self.MAJOR_EVENT[0]:
+                    self.downtimes.append((nights, self.MAJOR_EVENT[1], self.MAJOR_EVENT[2]))
+                    nights += self.MAJOR_EVENT[1] + 1
+                    continue
+                else:
+                    prob = random.random()
+                    if prob < self.INTERMEDIATE_EVENT[0]:
+                        self.downtimes.append((nights, self.INTERMEDIATE_EVENT[1],
+                                               self.INTERMEDIATE_EVENT[2]))
+                        nights += self.INTERMEDIATE_EVENT[1] + 1
+                        continue
+                    else:
+                        prob = random.random()
+                        if prob < self.MINOR_EVENT[0]:
+                            self.downtimes.append((nights, self.MINOR_EVENT[1], self.MINOR_EVENT[2]))
+            nights += 1
+
+        self.log.info("Total unscheduled downtime: {} days in {} days.".format(self.total_downtime,
+                                                                               survey_length))
---
+++ tests/downtime/test_unscheduled_downtime.py
@@ -0,0 +1,44 @@
+try:
+    from unittest import mock
+except ImportError:
+    import mock
+import unittest
+
+from lsst.sims.ocs.downtime.unscheduled_downtime import UnscheduledDowntime
+
+class UnscheduledDowntimeTest(unittest.TestCase):
+
+    def setUp(self):
+        self.usdt = UnscheduledDowntime()
+
+    def check_downtime(self, downtime, night, duration, activity):
+        self.assertEqual(downtime[0], night)
+        self.assertEqual(downtime[1], duration)
+        self.assertEqual(downtime[2], activity)
+
+    def test_basic_information_after_creation(self):
+        self.assertEqual(self.usdt.seed, 1640995200)
+        self.assertEqual(len(self.usdt), 0)
+
+    def test_information_after_iniitialization(self):
+        self.usdt.initialize()
+        self.assertEqual(len(self.usdt), 95)
+        self.assertEqual(self.usdt.total_downtime, 212)
+        self.check_downtime(self.usdt.downtimes[0], 7, 1, "minor event")
+        self.check_downtime(self.usdt.downtimes[-1], 3605, 7, "major event")
+
+    def test_alternate_survey_length(self):
+        self.usdt.initialize(survey_length=7300)
+        self.assertEqual(len(self.usdt), 176)
+        self.assertEqual(self.usdt.total_downtime, 376)
+        self.check_downtime(self.usdt.downtimes[0], 7, 1, "minor event")
+        self.check_downtime(self.usdt.downtimes[-1], 7285, 1, "minor event")
+
+    @mock.patch("time.time")
+    def test_alternate_seed(self, mock_time):
+        mock_time.return_value = 1466094470
+        self.usdt.initialize(random_seed=True)
+        self.assertEqual(len(self.usdt), 86)
+        self.assertEqual(self.usdt.total_downtime, 166)
+        self.check_downtime(self.usdt.downtimes[0], 28, 1, "minor event")
+        self.check_downtime(self.usdt.downtimes[-1], 3615, 1, "minor event")
