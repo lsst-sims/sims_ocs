f16dc3f58157fe38e64f4041cd4d4fc25f37119c
Michael Reuter <mareuter@lsst.org>
2016-07-21 15:41:51 -0700

    Wiring topic in for publishing.

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,6 +1,6 @@
 """Number of SAL put calls used in the configuration communicator.
 """
-CONFIG_COMM_PUT_CALLS = 10
+CONFIG_COMM_PUT_CALLS = 11
 """Number of currently defined area distribution proposals.
 """
 NUM_AREA_DIST_PROPS = 3
