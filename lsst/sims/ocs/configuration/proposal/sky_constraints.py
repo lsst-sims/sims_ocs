c1ec63c1cf98a827b6668e5ba7bed2b3aeaeba0c
Michael Reuter <mareuter@lsst.org>
2016-10-12 11:05:22 -0700

    Making and sending proposal cloud constraint.

--- lsst/sims/ocs/configuration/proposal/sky_constraints.py
+++ lsst/sims/ocs/configuration/proposal/sky_constraints.py
@@ -7,8 +7,10 @@ class SkyConstraints(pexConfig.Config):
     """
 
     max_airmass = pexConfig.Field('The maximum airmass allowed for any field.', float)
+    max_cloud = pexConfig.Field('The maximum fraction of clouds allowed for any field.', float)
 
     def setDefaults(self):
         """Default specification for sky constarints.
         """
         self.max_airmass = 2.5
+        self.max_cloud = 0.7
