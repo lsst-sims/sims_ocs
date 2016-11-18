e708a5a4031ac8c91d0dd6429d57037ef774fcae
Michael Reuter <mareuternh@gmail.com>
2016-11-18 13:34:37 -0700

    SAL/DDS working in container.

---
+++ docker/opsim4/startup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export LSST_DDS_DOMAIN=SOCS-DOCKER
+export NDDS_DISCOVERY_PEERS=builtin.shmem://
+export OPENSPLICE_LOC=${HOME}/repos/ts_opensplice/OpenSpliceDDS/V6.4.1/HDE/x86_64.linux
+export OSPL_URI=file://${OPENSPLICE_LOC}/etc/config/ospl.xml
+export SCHED_TOPIC_LIB=${HOME}/dds/lib
+export LD_LIBRARY_PATH=${OPENSPLICE_LOC}/lib:${SCHED_TOPIC_LIB}:${LD_LIBRARY_PATH}
+export PYTHONPATH=${SCHED_TOPIC_LIB}:${PYTHONPATH}
+/bin/bash
