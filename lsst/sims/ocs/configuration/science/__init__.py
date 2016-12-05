d2ed2c6095d5abf3572ce65b14784afdd56d116f
Michael Reuter <mareuter@lsst.org>
2016-12-05 10:23:59 -0700

    Removing Weak Lensing proposal.

--- lsst/sims/ocs/configuration/science/__init__.py
+++ lsst/sims/ocs/configuration/science/__init__.py
@@ -5,5 +5,4 @@ Module for classes that handle the actual specification of the science proposals
 from galactic_plane import *
 from north_ecliptic_spur import *
 from south_celestial_pole import *
-from weak_lensing import *
 from wide_fast_deep import *
--- tests/helpers.py
+++ tests/helpers.py
@@ -3,7 +3,7 @@
 CONFIG_COMM_PUT_CALLS = 11
 """Current list of all general proposals.
 """
-GEN_PROPS = ["GalacticPlane", "NorthEclipticSpur", "SouthCelestialPole", "WeakLensing", "WideFastDeep"]
+GEN_PROPS = ["GalacticPlane", "NorthEclipticSpur", "SouthCelestialPole", "WideFastDeep"]
 """Number of currently defined general proposals.
 """
 NUM_GEN_PROPS = len(GEN_PROPS)
