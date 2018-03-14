c1074267f30b90e52424b49dbea0641339a1be92
Andrew Heyer <aheyer@lsst.org>
2018-03-14 10:20:37 -0700

    Remove environment (preparing for submodule)

--- python/lsst/sims/ocs/environment/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Module for classes dealing with environmental conditions.
-"""
-from .cloud_model import *
-from .seeing_model import *
-
-from .scheduled_downtime import *
-from .unscheduled_downtime import *
